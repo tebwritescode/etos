eb1512c6d95cb7a7b8beaa8c0ba4438963d50c47
tebwritescode <93065214+tebwritescode@users.noreply.github.com>
2024-07-12 22:46:40 -0400

    Bugfix in etos.sh
    -Fixed a bug where if multiple email attachments had the same name, attachment-downloader would overwrite them and only process the last one
    
    -Removed the change directory from the for loop as it was causing issues
    
    -Updated attachment-downloader to use a template for naming downloaded attachments to avoid overwriting files if there are attachments with the same name
    
    -Changed variables to separate the base file path from the fullpath to allow for easier processing in the for loop
    
    Signed-off-by: tebwritescode <93065214+tebwritescode@users.noreply.github.com>

--- etos.sh
+++ etos.sh
@@ -1,47 +1,64 @@
 #!/bin/bash
-#Create a function for logging
+# Create a function for logging
 log() {
-	echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a /var/log/etos.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a /var/log/etos.log
 }
+
 log "#######################################################################################" || echo "#######################################################################################"
 log "Starting ETOS Importer" || echo "Starting ETOS Importer"
 log "Step 1: Check for and download attachments from email" || echo "Step 1: Check for and download attachments from email"
-attachment-downloader --host <EMAIL_SMTP_HOST> --username <EMAIL_ADDRESS> --password <EMAIL_PASSWORD> --imap-folder <EMAIL_FOLDER> --output <EMAIL_OUTPUT_LOCATION> --delete --subject-regex=<EMAIL_SUBJECT_FILTER>
+
+# Download attachments and rename them to avoid overwriting
+attachment-downloader --host <EMAIL_SMTP_HOST> --username <EMAIL_ADDRESS> --password <EMAIL_PASSWORD> --imap-folder <EMAIL_FOLDER> --output <EMAIL_OUTPUT_LOCATION> --delete --subject-regex=<EMAIL_SUBJECT_FILTER> --filename-template="{{date.strftime('%Y-%m-%d-%H-%M-%S-%f')}}_{{ attachment_name }}"
+
 log "Step 2: Declare file array" || echo "Step 2: Declare variables"
-declare -a filenames >> /var/log/etos.log
+declare -a filenames
+declare -a basenames
 index=0
+
 log "Step 3: Change working directory." || echo "Step 3: Change working directory."
-cd <EMAIL_OUTPUT_LOCATION>
+cd /opt/etos/tmp
+
 log "Step 4: Find all files in working directory" || echo "Step 4: Find all files in working directory"
-for file in *; do
-        if [[ -f "$file" && "$file" == *.pdf ]]; then
-                filenames[index]=$(realpath "$file") >> /var/log/etos.log
-                index=$((index + 1)) >> /var/log/etos.log
-        fi
+for file in *.pdf; do
+    if [[ -f "$file" ]]; then
+        fullpath=$(realpath "$file")
+	filebase=$(basename "$file" .${file##*.})
+        filenames[index]="$fullpath"
+	basenames[index]="$filebase"
+        log "Found file: $filebase"
+        index=$((index + 1))
+    fi
 done
+
 log "Step 5: Exit if no files found" || echo "Step 5: Exit if no files found"
 if [ ${#filenames[@]} -eq 0 ]; then
-        echo "Exit: No eligible imports found" || log "Exit: No eligible imports found"
-        exit 1
+    echo "Exit: No eligible imports found" || log "Exit: No eligible imports found"
+    exit 1
 fi
+
+log "Files found: ${filenames[*]}" || echo "Files found: ${filenames[*]}"
 log "Step 6: Loop through all files found" || echo "Step 6: Loop through all files found"
+log "Changing working directory to <LOCATION_OF_OBSIDIAN_VAULT>" || echo "Changing working directory to <LOCATION_OF_OBSIDIAN_VAULT>"
+cd <LOCATION_OF_OBSIDIAN_VAULT>
 for ((i=0; i < ${#filenames[@]}; i++)); do
-	log "Processing: ${filenames[$i]}" || echo "Processing: ${filenames[$i]}"
-        log "Converting ${filenames[$i]} to txt" || echo "Converting ${filenames[$i]} to txt"
-	pdftotext "${filenames[$i]}" >> /var/log/etos.log
-	textfile="${filenames[${i}]%.pdf}.txt"
-        log "Txt file is: ${textfile}" || echo "Txt file is: ${textfile}"
-	log "Change working directory" || echo "Change working directory"
-	cd <LOCATION_OF_OBSIDIAN_VAULT>
-        log "Send ${textfile} to ollama for summarizing" || echo "Send ${textfile} to ollama for summarizing"
-	log "Ollama output>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" || echo "Ollama output>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-	cat "${textfile}" | fabric --model llama3:latest --temp 0 --pattern write_code --remoteOllamaServer <REMOTE_OLLAMA_SERVER> | save New >> /var/log/etos.log
-	log "Removing temporary files" || echo "Removing temporary files"
-	rm "${filenames[$i]}" >> /var/log/etos.log
-        rm "${textfile}" >> /var/log/etos.log
+    fullpath="${filenames[$i]}"
+    basename="${basenames[$i]}"
+    log "Processing: $fullpath" || echo "Processing: $fullpath"
+    log "Converting $fullpath to txt" || echo "Converting $fullpath to txt"
+    pdftotext "$fullpath" >> /var/log/etos.log
+    textfile="${basename}.txt"
+    log "Txt file is: $textfile" || echo "Txt file is: $textfile"
+    log "Send /opt/etos/tmp/$textfile to ollama for summarizing" || echo "Send /opt/etos/tmp/$textfile to ollama for summarizing"
+    log "Ollama output>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" || echo "Ollama output>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    cat "/opt/etos/tmp/$textfile" | fabric --model llama3:latest --temp 0 --pattern write_code --remoteOllamaServer <REMOTE_OLLAMA_SERVER>:11434 | save New >> /var/log/etos.log
+    log "Removing temporary files" || echo "Removing temporary files"
+    rm "/opt/etos/tmp/$textfile" >> /var/log/etos.log
+    rm "$fullpath" >> /var/log/etos.log
 done
+
 log "Cleaning up" || echo "Cleaning up"
-rm <EMAIL_OUTPUT_LOCATION>/*.txt >> /var/log/etos.log
-rm <EMAIL_OUTPUT_LOCATION>/*.pdf >> /var/log/etos.log
-#Stop logging
+rm -f /opt/etos/tmp/*.txt >> /var/log/etos.log
+rm -f /opt/etos/tmp/*.pdf >> /var/log/etos.log
+# Stop logging
 log "#######################################################################################" | echo "#######################################################################################"
